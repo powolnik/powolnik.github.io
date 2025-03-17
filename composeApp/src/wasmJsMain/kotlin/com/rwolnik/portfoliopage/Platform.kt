d7fca9c896cb3060ed5ba70e0d70a44c9ff0c2c4
Radosław Wolnik <60506818+powolnik@users.noreply.github.com>
2025-03-17 17:20:05 +0100

    refresh

--- composeApp/src/wasmJsMain/kotlin/com/rwolnik/portfoliopage/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.rwolnik.portfoliopage
-
-class WasmPlatform {
-    val name: String = "Web with Kotlin/Wasm"
-}
-
-fun getPlatform() = WasmPlatform()
--- composeApp/src/wasmJsMain/kotlin/com/rwolnik/portfoliopage/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.rwolnik.portfoliopage.navigation
-
-sealed class Screen(val route: String) {
-    object Home : Screen("home")
-    object Projects : Screen("projects")
-    object Skills : Screen("skills")
-    object Contact : Screen("contact")
-}
