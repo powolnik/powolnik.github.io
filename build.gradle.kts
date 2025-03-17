72efd6e9ee65906ada26e914327e424e1af15771
Radosław Wolnik <60506818+powolnik@users.noreply.github.com>
2025-03-17 13:06:14 +0100

    Gradle env

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,5 @@
 plugins {
-    id 'com.android.application'
-    id 'kotlin-android'
-}
-
-android {
-    compileSdk 33
-    defaultConfig {
-        applicationId "com.example.app"
-        minSdk 21
-        targetSdk 33
-        versionCode 1
-        versionName "1.0"
-    }
-    buildTypes {
-        release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
-        }
-    }
-}
-
-dependencies {
-    implementation("androidx.compose.ui:ui:1.4.3")
-    implementation("androidx.compose.material3:material3:1.2.0")
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    kotlin("plugin.serialization") version "2.0.21"
 }
